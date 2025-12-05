bf7eac97bc3fe726c14a2e207a136227934081f8
yangeunjun00 <s25027@gsm.hs.kr>
2025-12-05 11:49:25 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+a,b,v = map(int,input().split())
+if a>v:
+    print(1)
+if (v-a)%(a-b)==0:
+    print((v-a)//(a-b)+1)
+else:
+    print((v-a)//(a-b)+2)
+
+    
+    
